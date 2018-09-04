64ef0a2eaef81586daa66c1b0c2fcf345d092b5a
Miss Araquel <missaraquel@gmail.com>
2018-09-04 13:29:01 -0700

    Initial files and instructions

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+//Import (require) connection.js into orm.js
+//Create the methods that will execute the necessary MySQL commands in the controllers
+// selectAll()
+// insertOne()
+// updateOne()
+// Export the ORM object in module.exports.
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,2 @@
+//Import express and burger.js 
+//Create the router for the app, and export the router at the end of your file.
