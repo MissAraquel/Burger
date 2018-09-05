77ea1bb2c36fd37f99645333a0202e6760fd1549
Miss Araquel <missaraquel@gmail.com>
2018-09-04 17:09:07 -0700

    adding schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
 --write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Hamburger', false), ('Cheeseburger', false), ('Bacon Cheeseburger', false), ('Veggie Burger', false);
