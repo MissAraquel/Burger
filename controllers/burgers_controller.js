846c7c2f24f8575a5d647a8c7d39468296209166
Miss Araquel <missaraquel@gmail.com>
2018-09-09 15:46:06 -0700

    adding burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,35 @@
 //Import express and burger.js 
 //Create the router for the app, and export the router at the end of your file.
+
+var express = require("express");
+
+var router = express.Router();
+
+//Import burger model to use its database functions.
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    res.redirect("/index");
+});
+
+router.get("/index", function (req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = { burgers: data };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/add/burger", function (req, res) {
+    burger.insertOne(req.body.burger_name, function() {
+        res.redirect("/index");
+    });
+});
+
+router.post("/burger/eat/:id", function (req, res) {
+    burger.updateOne(req.params.id, function() {
+        res.redirect("/index");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
