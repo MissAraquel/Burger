7bbfdd913e439e13da29304c08b89d0b4d9ff7f2
Miss Araquel <missaraquel@gmail.com>
2018-09-09 15:26:35 -0700

    adding burger model

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,28 @@
 //Import orm.js into burger.js
 //Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 //Export at the end of the burger.js file.
+
+var orm = require("../config/orm.js");
+
+var burger = {
+
+    selectAll: function(callback) {
+        orm.selectAll(function(res) {
+            callback(res);
+        });
+    },
+
+    insertOne: function(burger_name, callback) {
+        orm.insertOne(burger_name, function(res) {
+            callback(res);
+        });
+    },
+
+    updateOne: function(burger_id, callback) {
+        orm.updateOne(burger_id, function (res) {
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger; 
