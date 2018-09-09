0dda4aa3752c5ab4abaaf3d73e35cae514409069
Miss Araquel <missaraquel@gmail.com>
2018-09-09 15:10:55 -0700

    adding orm

--- config/orm.js
+++ config/orm.js
@@ -4,3 +4,36 @@
 // insertOne()
 // updateOne()
 // Export the ORM object in module.exports.
+
+var connection = require("../config/connection.js");
+
+var orm = {
+
+    selectAll: function(cb) {
+        connection.query("SELECT * FROM burgers", function(err, res) {
+            if (err) throw err; 
+            cb(res);
+        });
+    },
+
+    insertOne: function(burger_name, cb) {
+        connection.query("INSERT INTO burgers SET ?", {
+            burger_name: burger_name,
+            devoured: false,
+        }, function(err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+
+    updateOne: function(burgerID, cb) {
+        connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}, {id: burgerID}],
+        function(err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    }
+
+};
+
+module.exports = orm;
